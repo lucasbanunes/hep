f2d37f5dcabed5a9143090cff2cb7695b8d73bc0
lucasbanunes <lucasbanunes@gmail.com>
2024-08-06 06:37:12 -0400

    Added nrows param to hep_utils.root.rdf_to_pandas

--- hep_utils/root.py
+++ hep_utils/root.py
@@ -20,7 +20,9 @@ def get_column_names(rdf: RDataFrame) -> List[str]:
     return [str(col) for col in rdf.GetColumnNames()]
 
 
-def rdf_to_pandas(rdf: RDataFrame, columns: Optional[List[str]] = None) -> pd.DataFrame:
+def rdf_to_pandas(rdf: RDataFrame,
+                  columns: Optional[List[str]] = None,
+                  nrows: int = -1) -> pd.DataFrame:
     """
     Convert a RDataFrame to a pandas DataFrame.
 
@@ -30,6 +32,10 @@ def rdf_to_pandas(rdf: RDataFrame, columns: Optional[List[str]] = None) -> pd.Da
         The RDataFrame object.
     columns: Optional[List[str]]
         The columns to convert. By default, all columns are converted.
+    nrows : int, optional
+        Number of rows to convert. By default, all rows are converted.
+        This param does not avoid loading all the data in memory, it just
+        slices the data. By default -1.
 
     Returns
     -------
@@ -42,7 +48,11 @@ def rdf_to_pandas(rdf: RDataFrame, columns: Optional[List[str]] = None) -> pd.Da
         pdf_dict = rdf.AsNumpy(columns)
     keys = list(pdf_dict.keys())
     for key in keys:
-        # this is necessary because the keys are of type std::string (cppyy.gbl.std.string)
+        # this is necessary because the keys are of type
+        # std::string (cppyy.gbl.std.string)
         # and pandas don't know how to deal with it
-        pdf_dict[str(key)] = pdf_dict.pop(key)
+        if nrows < 0:
+            pdf_dict[str(key)] = pdf_dict.pop(key)
+        else:
+            pdf_dict[str(key)] = pdf_dict.pop(key)[:nrows]
     return pd.DataFrame.from_dict(pdf_dict)
